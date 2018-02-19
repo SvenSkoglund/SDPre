f1e00c0a35d146b44cc0da405c0f8b2e9a3795c5
Sven <svnerskoglund@gmail.com>
2018-02-19 14:51:54 -0700

    Update to exercise 1

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg{
   public static void main(String [] args){
-    System.out.println("I am determined to learn code");
-    System.out.println("Today is a good day");
+    System.out.println("I, Sven Skoglund, am determined to learn code");
+    System.out.println("Today is Monday, February 28th, 2018");
   }
 }
