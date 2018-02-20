06d60c88919a637e4b5a878c1e1b31fff1a0ce43
Sven <svnerskoglund@gmail.com>
2018-02-20 15:15:00 -0700

    Exercises 5-6

---
+++ JavaTheHardWay/CreatingVariables.java
@@ -0,0 +1,28 @@
+public class CreatingVariables{
+  public static void main( String [] args ) {
+    int x , y ,age;
+    double seconds, e, checking;
+    String firstName, lastName, title;
+
+    x = 10;
+    y = 400;
+    age = 39;
+
+    seconds = 4.71;
+    e = 2.71828182845904523536;
+    checking = 1.89;
+
+    firstName = "Sven";
+    lastName = "Skoglund";
+    title = "Mr.";
+
+    System.out.println("The vatiable x contains" + x);
+    System.out.println("The value " + y + " is stored in y");
+    System.out.println("The experiment took "+seconds+ " seconds");
+    System.out.println("A favorite irrational # is Euler's number "+ e);
+    System.out.println("Hopefully you have more that $"+checking+"!");
+    System.out.println("My name's " + title + " " + firstName + " " + lastName);
+    
+
+  }
+}
