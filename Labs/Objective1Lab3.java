90e0f06ccbd847b35a2f6b678f24e195e0266f59
Sven <svnerskoglund@gmail.com>
2018-02-20 14:49:34 -0700

    Labs 1-3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,8 @@
+public class Objective1Lab3 {
+  public static void main (String [] args){
+    System.out.println("Name : Sven Skoglund");
+    System.out.println("Age : 25");
+    System.out.println("State : Colorado");
+
+  }
+}
